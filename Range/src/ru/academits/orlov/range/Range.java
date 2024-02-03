8e3b9cb1b116bb8023fe58382f44284194574cf1
lapoosick <alka03@mail.ru>
2024-02-03 22:19:47 +0700

    Initial commit.

---
+++ Range/src/ru/academits/orlov/range/Range.java
@@ -0,0 +1,4 @@
+package ru.academits.orlov.range;
+
+public class Range {
+}
---
+++ Range/src/ru/academits/orlov/range_main/Main.java
@@ -0,0 +1,13 @@
+package ru.academits.orlov.range_main;
+
+import ru.academits.orlov.range.Range;
+
+public class Main {
+    public static void main(String[] args) {
+        Range range = new Range();
+        Range range1 = new Range();
+        Range range2 = new Range();
+
+
+    }
+}
