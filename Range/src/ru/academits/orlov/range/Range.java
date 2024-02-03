131dc494c1981c5da88bf9c20a9587c80f7b32d7
lapoosick <alka03@mail.ru>
2024-02-03 22:24:38 +0700

    Написаны классы Range и Main в модуль Range.

--- Range/src/ru/academits/orlov/range/Range.java
+++ Range/src/ru/academits/orlov/range/Range.java
@@ -1,4 +1,116 @@
 package ru.academits.orlov.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && number <= to;
+    }
+
+    public Range getRangesIntersection(Range otherRange) {
+        double otherFrom = otherRange.getFrom();
+        double otherTo = otherRange.getTo();
+
+        if (to <= otherFrom || otherTo <= from) {
+            return null;
+        }
+
+        if (from <= otherFrom) {
+            if (to >= otherTo) {
+                return otherRange;
+            }
+
+            return new Range(otherFrom, to);
+        } else {
+            if (otherTo < to) {
+                return new Range(from, otherTo);
+            }
+        }
+
+        return this;
+    }
+
+    public Range[] getRangesUnion(Range otherRange) {
+        double otherFrom = otherRange.getFrom();
+        double otherTo = otherRange.getTo();
+
+        if (from <= otherFrom) {
+            if (otherTo <= to) {
+                return new Range[]{this};
+            }
+
+            if (otherFrom <= to) {
+                return new Range[]{new Range(from, otherTo)};
+            }
+        } else {
+            if (to < otherTo) {
+                return new Range[]{otherRange};
+            }
+
+            if (from <= otherTo) {
+                return new Range[]{new Range(otherFrom, to)};
+            }
+        }
+
+        return new Range[]{this, otherRange};
+    }
+
+    public Range[] getRangesSubtraction(Range otherRange) {
+        double otherFrom = otherRange.getFrom();
+        double otherTo = otherRange.getTo();
+
+        if (from == otherFrom) {
+            if (otherTo < to) {
+                return new Range[]{new Range(otherTo, to)};
+            }
+
+            return null;
+        }
+
+        if (from < otherFrom) {
+            if (otherTo < to) {
+                return new Range[]{new Range(from, otherFrom), new Range(otherTo, to)};
+            }
+
+            if (otherFrom < to) {
+                return new Range[]{new Range(from, otherFrom)};
+            }
+        } else {
+            if (to <= otherTo) {
+                return null;
+            }
+
+            if (from < otherTo) {
+                return new Range[]{new Range(otherTo, to)};
+            }
+        }
+
+        return new Range[]{this};
+    }
 }
--- Range/src/ru/academits/orlov/range_main/Main.java
+++ Range/src/ru/academits/orlov/range_main/Main.java
@@ -2,12 +2,78 @@ package ru.academits.orlov.range_main;
 
 import ru.academits.orlov.range.Range;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range();
-        Range range1 = new Range();
-        Range range2 = new Range();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Введите начало диапазона: ");
+        double from = scanner.nextDouble();
 
+        System.out.print("Введите конец диапазона: ");
+        double to = scanner.nextDouble();
+
+        System.out.print("Введите число для проверки: ");
+        double number = scanner.nextDouble();
+
+        Range range = new Range(from, to);
+
+        System.out.printf("Длина диапазона равна: %f.%n", range.getLength());
+        System.out.println(range.isInside(number) ? "Ваше число в диапазоне." : "Ваше число вне диапазона.");
+
+        range.setFrom(1.6574);
+        range.setTo(6.28749);
+
+        System.out.printf("Новые границы диапазона: %f и %f.%n", range.getFrom(), range.getTo());
+        System.out.printf("Новая длина диапазона: %f.", range.getLength());
+        System.out.println();
+
+        System.out.print("Укажите начало первого интервала: ");
+        double fromA = scanner.nextDouble();
+
+        System.out.print("Укажите конец первого интервала: ");
+        double toA = scanner.nextDouble();
+
+        System.out.print("Укажите начало второго интервала: ");
+        double fromB = scanner.nextDouble();
+
+        System.out.print("Укажите конец второго интервала: ");
+        double toB = scanner.nextDouble();
+
+        Range rangeA = new Range(fromA, toA);
+        Range rangeB = new Range(fromB, toB);
+
+        Range rangesIntersection = rangeA.getRangesIntersection(rangeB);
+
+        if (rangesIntersection == null) {
+            System.out.println("Интервалы не пересекаются.");
+        } else {
+            System.out.println("Интервал-пересечение: (" + rangesIntersection.getFrom() + ", " + rangesIntersection.getTo() + ")");
+        }
+
+        Range[] rangesUnion = rangeA.getRangesUnion(rangeB);
+
+        System.out.print("Результат объединения двух интервалов: (" + rangesUnion[0].getFrom() + ", " + rangesUnion[0].getTo() + ")");
+
+        if (rangesUnion.length > 1) {
+            System.out.println(" + (" + rangesUnion[1].getFrom() + ", " + rangesUnion[1].getTo() + ")");
+        } else {
+            System.out.println();
+        }
+
+        Range[] rangeSubtraction = rangeA.getRangesSubtraction(rangeB);
+
+        if (rangeSubtraction == null) {
+            System.out.println("Разностью двух интервалов является пустое множество.");
+
+            return;
+        }
+
+        System.out.print("Разность двух интервалов: (" + rangeSubtraction[0].getFrom() + ", " + rangeSubtraction[0].getTo() + ")");
+
+        if (rangeSubtraction.length > 1) {
+            System.out.println(" + (" + rangeSubtraction[1].getFrom() + ", " + rangeSubtraction[1].getTo() + ")");
+        }
     }
 }
