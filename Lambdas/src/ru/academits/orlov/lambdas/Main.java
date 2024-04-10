08821c9f0931a6126b42c8320ae8c76e7cef80b8
lapoosick <alka03@mail.ru>
2024-04-10 09:55:52 +0700

    Lambdas: исправлено согласно замечаниям преподавателя.

--- Lambdas/src/ru/academits/orlov/lambdas/Main.java
+++ Lambdas/src/ru/academits/orlov/lambdas/Main.java
@@ -1,32 +1,27 @@
-package ru.academits.orlov.lambdas_main;
-
-import ru.academits.orlov.lambdas.Person;
+package ru.academits.orlov.lambdas;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person("Сергей", 8);
-        Person person2 = new Person("Иван", 4);
-        Person person3 = new Person("Антон", 34);
-        Person person4 = new Person("Антон", 28);
-        Person person5 = new Person("Пётр", 20);
-        Person person6 = new Person("Пётр", 26);
-
-        Supplier<Stream<Person>> personStreamSupplier = () -> Stream.of(person1, person2, person3, person4, person5, person6);
+        List<Person> personsList = List.of(
+                new Person("Сергей", 8),
+                new Person("Иван", 4),
+                new Person("Антон", 34),
+                new Person("Антон", 28),
+                new Person("Пётр", 20),
+                new Person("Пётр", 26));
 
         // получить список уникальных имен
-        List<String> uniqueNamesList = personStreamSupplier.get()
+        List<String> uniqueNamesList = personsList.stream()
                 .map(Person::getName)
                 .distinct()
                 .toList();
 
-        Set<String> uniqueNamesSet = personStreamSupplier.get()
+        Set<String> uniqueNamesSet = personsList.stream()
                 .map(Person::getName)
                 .collect(Collectors.toSet());
 
@@ -35,14 +30,16 @@ public class Main {
         System.out.println(uniqueNamesSet.stream().collect(Collectors.joining(", ", "Имена: ", ".")));
 
         // получить список людей младше 18, посчитать для них средний возраст
-        double personsUnder18AverageAge = personStreamSupplier.get()
+        double personsUnder18AverageAge = personsList.stream()
                 .filter(person -> person.getAge() < 18)
                 .collect(Collectors.averagingInt(Person::getAge));
 
-        System.out.println("Средний возраст людей младше 18: " + personsUnder18AverageAge);
+        System.out.println(personsUnder18AverageAge > 0
+                ? "Средний возраст людей младше 18: " + personsUnder18AverageAge
+                : "В списке нет людей младше 18.");
 
         // при помощи группировки получить Мар, в котором ключи - имена, а значения - средний возраст
-        Map<String, Double> averageAgesByName = personStreamSupplier.get()
+        Map<String, Double> averageAgesByName = personsList.stream()
                 .collect(Collectors.groupingBy(
                         Person::getName,
                         Collectors.averagingInt(Person::getAge)));
@@ -50,7 +47,7 @@ public class Main {
         System.out.println("Имена со средним возрастом их обладателей: " + averageAgesByName);
 
         // получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
-        personStreamSupplier.get()
+        personsList.stream()
                 .filter(person -> person.getAge() >= 20 && person.getAge() <= 45)
                 .sorted((p1, p2) -> p2.getAge() - p1.getAge())
                 .map(Person::getName)
